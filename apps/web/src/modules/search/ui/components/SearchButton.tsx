b5ec864cbafd3139b12210ed49f81bb4a5cb86ae
wkflanders <wflan@seas.upenn.edu>
2025-06-25 10:45:23 -0400

    feat: add mobile Searchbar components

---
+++ apps/web/src/modules/search/ui/components/SearchButton.tsx
@@ -0,0 +1,17 @@
+import { Search } from "lucide-react";
+import { useState } from "react";
+import { SearchDialog } from "./SearchDialog";
+import { Button } from "@/components/ui/button";
+
+export const SearchButton = () => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    return (
+        <div className="md:hidden">
+            <Button className="size-7 px-2" onClick={() => setOpen(true)}>
+                <Search />
+            </Button>
+            <SearchDialog open={open} onOpenChange={setOpen} />
+        </div>
+    );
+};
