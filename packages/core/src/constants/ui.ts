272eb7cfd3d1dfd27b6a2238c4ac0ae8a96e6c43
wkflanders <wflan@seas.upenn.edu>
2025-06-28 05:30:49 -0400

    feat: add ui constants to core

---
+++ packages/core/src/constants/ui.ts
@@ -0,0 +1,6 @@
+export const SIDEBAR_ROUTES = [
+    { href: '/', label: 'Home', iconName: 'Home' },
+    { href: '/competitions', label: 'Competitions', iconName: 'Trophy' },
+    { href: '/market', label: 'Market', iconName: 'Store' },
+    { href: '/profile', label: 'Profile', iconName: 'User' },
+] as const;
