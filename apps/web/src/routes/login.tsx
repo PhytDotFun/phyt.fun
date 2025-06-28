3647ff8402a800b627f265ab75d020bfaff15d47
wkflanders <wflan@seas.upenn.edu>
2025-06-28 00:06:00 -0400

    feat: add login route

--- apps/web/src/routes/login.tsx
+++ apps/web/src/routes/login.tsx
@@ -1,13 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { Login } from "@/modules/layout/ui/components/Login";
+
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { Login } from "@/modules/auth/ui/components/Login";
 
 export const Route = createFileRoute('/login')({
+    beforeLoad: ({ context }) => {
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        if (context.authentication?.authenticated) {
+            throw redirect({ to: '/' });
+        }
+    },
     component: LoginPage,
 });
 
 function LoginPage() {
-
     return <Login />;
 }
-
-
