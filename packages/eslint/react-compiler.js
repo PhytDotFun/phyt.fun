c25e906b0721a617d052c360468257d4e7609f86
wkflanders <wflan@seas.upenn.edu>
2025-07-06 11:58:21 -0400

    feat: create rc eslint config and use in vite app

---
+++ packages/eslint/react-compiler.js
@@ -0,0 +1,46 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import * as reactCompiler from 'eslint-plugin-react-compiler';
+
+/**
+ * A custom ESLint configuration for the React Compiler.
+ *
+ * @type {import("eslint").Linter.Config[]} */
+export const reactCompilerConfig = [
+    js.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        languageOptions: {
+            ...pluginReact.configs.flat.recommended.languageOptions,
+            globals: {
+                ...globals.serviceworker,
+                ...globals.browser
+            }
+        }
+    },
+    {
+        plugins: {
+            'react-hooks': pluginReactHooks
+        },
+        settings: { react: { version: 'detect' } },
+        rules: {
+            ...pluginReactHooks.configs.recommended.rules,
+            // React scope no longer necessary with new JSX transform.
+            'react/react-in-jsx-scope': 'off'
+        }
+    },
+    {
+        plugins: {
+            'react-compiler': reactCompiler
+        },
+        rules: {
+            'react-compiler/react-compiler': 'error'
+        }
+    }
+];
