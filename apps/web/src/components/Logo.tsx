91a061543dd29561c67cd4411f08c3bd8f14b07a
wkflanders <wflan@seas.upenn.edu>
2025-07-05 22:34:29 -0400

    feat: add Logo component classname, type, and style props

--- apps/web/src/components/Logo.tsx
+++ apps/web/src/components/Logo.tsx
@@ -1,6 +1,27 @@
-export const HeaderLogo = () => {
-    const logoUrl =
-        'https://rsg5uys7zq.ufs.sh/f/AMgtrA9DGKkFQRL7lOy3oHCqsFTvcnXikarE2xmRz7bWfp5D';
+import { cn } from '@/lib/utils';
 
-    return <img src={logoUrl} alt="Phyt" className="w-30 min-w-30" />;
+interface LogoProps {
+    className?: string;
+    type?: 'header' | 'login';
+    hasDotFun?: boolean;
+}
+
+export const Logo = ({ className, type, hasDotFun }: LogoProps) => {
+    const defaultUrl =
+        'https://rsg5uys7zq.ufs.sh/f/AMgtrA9DGKkFCr2hRMJrJ6ohsvQUI1liN8BxWu0wqFTVAjcO';
+
+    const dotFunUrl =
+        'https://rsg5uys7zq.ufs.sh/f/AMgtrA9DGKkFfcbkzXzul2HmTFIo5PR3pxMSVjZOqJLeuYrn';
+
+    return (
+        <img
+            src={hasDotFun === true ? dotFunUrl : defaultUrl}
+            alt="Phyt"
+            className={cn(
+                type === 'header' && 'w-30 min-w-30',
+                type === 'login' && 'w-60 min-w-40',
+                className
+            )}
+        />
+    );
 };
