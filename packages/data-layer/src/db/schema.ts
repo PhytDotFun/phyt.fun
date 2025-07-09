3a2962d5bbfdb0989eab7ca03d211d3fbe5e9620
wkflanders <wflan@seas.upenn.edu>
2025-07-09 18:56:29 -0400

    fix: remove capitals from drizzle/pg col names

--- packages/data-layer/src/db/schema.ts
+++ packages/data-layer/src/db/schema.ts
@@ -15,26 +15,26 @@ export const users = pgTable(
     'users',
     {
         id: serial('id').primaryKey(),
-        privyDID: char('privyDID', { length: 35 }).notNull(),
+        privyDID: char('privy_did', { length: 35 }).notNull(),
         email: varchar('email', { length: 255 }).unique(),
         username: char('username', { length: 15 }).notNull(),
         role: role('role').notNull().default('user'),
-        stravaUsername: varchar('stravaUsername', { length: 255 }),
-        stravaID: integer('stravaID'),
-        profilePictureUrl: varchar('profilePictureUrl')
+        stravaUsername: varchar('strava_username', { length: 255 }),
+        stravaID: integer('strava_id'),
+        profilePictureUrl: varchar('profile_picture_url')
             .notNull()
             .default(
                 'https://rsg5uys7zq.ufs.sh/f/AMgtrA9DGKkFTTUitgzI9xWiHtmo3wu4PcnYaCGO1jX0bRBQ'
             ),
-        walletAddress: char('walletAddress', { length: 42 }).notNull(),
-        createdAt: timestamp('createdAt').notNull().defaultNow(),
-        updatedAt: timestamp('updatedAt').notNull().defaultNow(),
-        deletedAt: timestamp('deletedAt')
+        walletAddress: char('wallet_address', { length: 42 }).notNull(),
+        createdAt: timestamp('created_at').notNull().defaultNow(),
+        updatedAt: timestamp('updated_at').notNull().defaultNow(),
+        deletedAt: timestamp('deleted_at')
     },
     (table) => [
-        uniqueIndex('privyDIDIdx').on(table.privyDID),
-        uniqueIndex('emailIdx').on(table.email),
-        uniqueIndex('usernameIdx').on(table.username),
-        uniqueIndex('walletAddressIdx').on(table.walletAddress)
+        uniqueIndex('privy_did_idx').on(table.privyDID),
+        uniqueIndex('email_idx').on(table.email),
+        uniqueIndex('username_idx').on(table.username),
+        uniqueIndex('wallet_address_idx').on(table.walletAddress)
     ]
 );
