47a886979493956105389aab820dc6e3ae4b7b7f
wkflanders <wflan@seas.upenn.edu>
2025-07-12 22:25:46 -0400

    feat: build user procedures and user router

---
+++ packages/trpc-adapters/src/users/procedures.ts
@@ -0,0 +1,19 @@
+import { TRPCError } from '@trpc/server';
+import { UserProfileSchema } from '@phyt/data-persistence/db/models/users';
+
+import { router, protectedProcedure } from '@/trpc';
+
+import { UserService } from './service';
+
+export const userRouter = router({
+    me: protectedProcedure.query(async ({ ctx }) => {
+        const svc = new UserService(ctx);
+        const user = await svc.me(ctx.userId);
+        if (!user)
+            throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                message: 'Error fetching user'
+            });
+        return UserProfileSchema.parse(user);
+    })
+});
