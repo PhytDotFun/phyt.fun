6776b8f5a75e3092fe9c603873b509e7d4661645
wkflanders <wflan@seas.upenn.edu>
2025-07-13 18:33:19 -0400

    feat: init redis client

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,1 +1,20 @@
-export const name = 'redis';
+import Redis from 'ioredis';
+
+import { env } from './env';
+
+export const redis = new Redis(env.REDIS_URL, {
+    maxRetriesPerRequest: null,
+    enableReadyCheck: true
+});
+
+redis.on('error', (error) => {
+    console.error('[redis] ', error);
+});
+
+redis.on('connect', () => {
+    console.info('[redis] connected');
+});
+
+process.on('SIGINT', () => {
+    void redis.quit();
+});
