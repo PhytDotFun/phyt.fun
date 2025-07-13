894ca6c63df48bc86d76690c24fd63385feab6c0
wkflanders <wflan@seas.upenn.edu>
2025-07-12 22:32:39 -0400

    build: fix linter config applying and import rules on relative paths

--- packages/eslint/base.js
+++ packages/eslint/base.js
@@ -1,89 +1,112 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import globals from "globals";
-import importPlugin from "eslint-plugin-import";
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import turboPlugin from 'eslint-plugin-turbo';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import importPlugin from 'eslint-plugin-import';
 
 /**
  * A shared ESLint configuration for the repository.
  *
  * @type {import("eslint").Linter.Config[]}
  * */
-export const baseConfig = [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.strict,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
+const baseConfig = [
+    {
+        ignores: ['**/eslint.config.js', '**/tsup.config.ts']
     },
-  },
-  {
-    plugins: {
-      turbo: turboPlugin,
-    },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-    },
-  },
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      "import/extensions": [
-        "error",
-        "never",
-        {
-          json: "always",
+    js.configs.recommended,
+    ...tseslint.configs.strictTypeChecked.map((config) => ({
+        ...config,
+        files: ['**/*.{ts,tsx}']
+    })),
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: ['./tsconfig.json']
+            }
         },
-      ],
+        rules: {
+            '@typescript-eslint/no-non-null-assertion': 'warn',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+            ]
+        }
     },
-  },
-//   {
-//     plugins: {
-//       onlyWarn,
-//     },
-//   },
-  {
-    ignores: ["dist/**"],
-  },
+    {
+        plugins: {
+            import: importPlugin
+        },
+        settings: {
+            'import/resolver': {
+                typescript: {
+                    project: './tsconfig.json',
+                    alwaysTryTypes: true
+                },
+                node: true
+            }
+        },
+        rules: {
+            'import/order': [
+                'error',
+                {
+                    'newlines-between': 'always',
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        'parent',
+                        'sibling',
+                        'index'
+                    ]
+                }
+            ],
+            'import/extensions': [
+                'error',
+                'ignorePackages',
+                {
+                    js: 'never',
+                    jsx: 'never',
+                    ts: 'never',
+                    tsx: 'never',
+                    json: 'always'
+                }
+            ],
+            'import/no-relative-parent-imports': [
+                'error',
+                {
+                    ignore: ['^@/'] // Still throwing linting error with path alias
+                }
+            ]
+        }
+    },
+    {
+        files: ['**/*.js', '**/*.cjs'],
+        languageOptions: {
+            globals: {
+                ...globals.node
+            }
+        }
+    },
+    {
+        plugins: {
+            turbo: turboPlugin
+        },
+        rules: {
+            'turbo/no-undeclared-env-vars': 'warn'
+        }
+    },
+    //   {
+    //     plugins: {
+    //       onlyWarn,
+    //     },
+    //   },
+    {
+        ignores: ['dist/**']
+    },
+    eslintConfigPrettier
 ];
 
-
-/**
- * A shared ESLint configuration for the repository.
- *
- * @type {import("eslint").Linter.Config[]}
- * */
-export const baseConfigWithoutImport = [
-    js.configs.recommended,
-    eslintConfigPrettier,
-    ...tseslint.configs.strict,
-    {
-      languageOptions: {
-        globals: {
-          ...globals.node,
-        },
-      },
-    },
-    {
-      plugins: {
-        turbo: turboPlugin,
-      },
-      rules: {
-        "turbo/no-undeclared-env-vars": "warn",
-      },
-    },
-//   {
-//     plugins: {
-//       onlyWarn,
-//     },
-//   },
-    {
-      ignores: ["dist/**"],
-    },
-  ];
+export default baseConfig;
