ef46c2dd334f6a5d2fedc327aa64c032ae8eb94e
wkflanders <wflan@seas.upenn.edu>
2025-07-13 23:20:29 -0400

    fix: add default to redis url env var

--- packages/redis/src/env.ts
+++ packages/redis/src/env.ts
@@ -7,7 +7,7 @@ export const env = createEnv({
         NODE_ENV: z
             .enum(['development', 'production', 'test'])
             .default('development'),
-        REDIS_URL: z.string().url()
+        REDIS_URL: z.string().url().default('redis://localhost:6379')
     },
     runtimeEnv: process.env,
     emptyStringAsUndefined: true,
