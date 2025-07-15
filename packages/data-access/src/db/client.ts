5baff746fa198a1cc781d50c8df87c08aa7e026b
wkflanders <wflan@seas.upenn.edu>
2025-07-15 18:56:43 -0400

    feat: setup a pgClient (pool)

--- packages/data-access/src/db/client.ts
+++ packages/data-access/src/db/client.ts
@@ -1,5 +1,7 @@
+import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
 import { env } from '@/env';
 
+export const pgClient = new Pool({ connectionString: env.DATABASE_URL });
 export const db = drizzle(env.DATABASE_URL);
