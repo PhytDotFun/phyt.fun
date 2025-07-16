f61ffd478c565c50240aac0a87e1893835306a2c
wkflanders <wflan@seas.upenn.edu>
2025-07-16 18:40:17 -0400

    fix: rework useAuth hook to memoize login context w/in hook context

--- apps/web/src/hooks/use-auth.ts
+++ apps/web/src/hooks/use-auth.ts
@@ -1,21 +1,45 @@
-import { usePrivy } from '@privy-io/react-auth';
+import {
+    usePrivy,
+    useLoginWithOAuth,
+    type PrivyErrorCode
+} from '@privy-io/react-auth';
 import { useCallback, useMemo } from 'react';
 
-export const useAuth = () => {
+export const useAuth = ({
+    onSignInComplete,
+    onSignInError
+}: {
+    onSignInComplete?: () => void;
+    onSignInError?: (error: PrivyErrorCode) => void;
+} = {}) => {
     const { ready, authenticated, user, logout } = usePrivy();
+    const { initOAuth, loading } = useLoginWithOAuth({
+        onComplete: () => {
+            onSignInComplete?.();
+        },
+        onError: (error) => {
+            onSignInError?.(error);
+        }
+    });
 
     const signOut = useCallback(async () => {
         await logout();
     }, [logout]);
 
+    const signIn = useCallback(async () => {
+        await initOAuth({ provider: 'twitter' });
+    }, [initOAuth]);
+
     return useMemo(
         () => ({
+            signIn,
             signOut,
             authenticated,
             user,
-            ready
+            ready,
+            loading
         }),
-        [signOut, authenticated, user, ready]
+        [loading, signIn, signOut, authenticated, user, ready]
     );
 };
 
