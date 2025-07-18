0b6cc0336007c7d7bfbb3135b310020fba65ebab
wkflanders <wflan@seas.upenn.edu>
2025-07-18 09:53:22 -0400

    feat: user procedures for get by wa and privydid

--- packages/trpc-adapters/src/users/procedures.ts
+++ packages/trpc-adapters/src/users/procedures.ts
@@ -1,12 +1,13 @@
 import { TRPCError } from '@trpc/server';
 import { UserProfileSchema } from '@phyt/data-access/models/users';
+import { z } from 'zod';
 
 import { router, protectedProcedure } from '../trpc';
 
 import { UserService } from './service';
 
 export const userRouter = router({
-    me: protectedProcedure.query(async ({ ctx }) => {
+    getCurrentUser: protectedProcedure.query(async ({ ctx }) => {
         const svc = new UserService(ctx);
         if (!ctx.userId) {
             throw new TRPCError({
@@ -14,12 +15,55 @@ export const userRouter = router({
                 message: 'User ID not found in context'
             });
         }
-        const user = await svc.me(ctx.userId);
-        if (!user)
+        const currentUser = await svc.getUserByPrivyDID(ctx.userId);
+        if (!currentUser) {
             throw new TRPCError({
                 code: 'INTERNAL_SERVER_ERROR',
                 message: 'Error fetching user'
             });
-        return UserProfileSchema.parse(user);
-    })
+        }
+        return UserProfileSchema.parse(currentUser);
+    }),
+    getUserByPrivyDID: protectedProcedure
+        .input(z.string().startsWith('did:privy:'))
+        .query(async ({ ctx, input }) => {
+            const privyDID = input;
+
+            const svc = new UserService(ctx);
+            if (!ctx.userId) {
+                throw new TRPCError({
+                    code: 'UNAUTHORIZED',
+                    message: 'User ID not found in context'
+                });
+            }
+            const user = await svc.getUserByPrivyDID(privyDID);
+            if (!user) {
+                throw new TRPCError({
+                    code: 'INTERNAL_SERVER_ERROR',
+                    message: 'Error fetching user'
+                });
+            }
+            return UserProfileSchema.parse(user);
+        }),
+    getUserByWalletAddress: protectedProcedure
+        .input(z.string())
+        .query(async ({ ctx, input }) => {
+            const walletAddress = input;
+
+            const svc = new UserService(ctx);
+            if (!ctx.userId) {
+                throw new TRPCError({
+                    code: 'UNAUTHORIZED',
+                    message: 'User ID not found in context'
+                });
+            }
+            const user = await svc.getUserByWalletAddress(walletAddress);
+            if (!user) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: 'User not found'
+                });
+            }
+            return UserProfileSchema.parse(user);
+        })
 });
