b025540eaf5d781188004866a09459a9af8cf073
wkflanders <wflan@seas.upenn.edu>
2025-07-21 08:57:36 -0400

    fix: update di for workers

--- apps/workers/src/di.ts
+++ apps/workers/src/di.ts
@@ -2,13 +2,16 @@ import type { Dependencies } from '@phyt/core/di';
 import { db } from '@phyt/data-access/db/client';
 import { getRedisClient } from '@phyt/redis/client';
 import { PrivyClient } from '@privy-io/server-auth';
+import { createAppDependencies } from '@phyt/trpc-adapters/di';
 
 import { env } from './env';
 
 const privy = new PrivyClient(env.PRIVY_APP_ID, env.PRIVY_SECRET_KEY);
 
-export const dependencies: Dependencies = {
+const coreDeps: Dependencies = {
     db,
     redis: getRedisClient(),
     privy
 };
+
+export const appDeps = createAppDependencies(coreDeps);
