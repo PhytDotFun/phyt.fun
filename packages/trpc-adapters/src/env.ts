4d8f572fb38f298c93bde3f455188262684e8953
wkflanders <wflan@seas.upenn.edu>
2025-07-21 08:54:43 -0400

    build: add SALTS for id encoding

--- packages/trpc-adapters/src/env.ts
+++ packages/trpc-adapters/src/env.ts
@@ -6,7 +6,12 @@ export const env = createEnv({
     server: {
         NODE_ENV: z
             .enum(['development', 'production', 'test'])
-            .default('development')
+            .default('development'),
+        POSTS_SALT: z.string(),
+        COMMENTS_SALT: z.string(),
+        REACTIONS_SALT: z.string(),
+        RUNS_SALT: z.string(),
+        USERS_SALT: z.string()
     },
     runtimeEnv: process.env,
     emptyStringAsUndefined: true,
