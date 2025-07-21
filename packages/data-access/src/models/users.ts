3a6490fd38e7cb59736538e77d79a80d373dd394
wkflanders <wflan@seas.upenn.edu>
2025-07-21 08:53:20 -0400

    fix: separate services schemas/models from repository schemas/models

--- packages/data-access/src/models/users.ts
+++ packages/data-access/src/models/users.ts
@@ -4,16 +4,16 @@ import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
 import { users } from '../db/schema';
 
-export type User = InferSelectModel<typeof users>;
-export type NewUser = InferInsertModel<typeof users>;
+export type SelectUser = InferSelectModel<typeof users>;
+export type InsertUser = InferInsertModel<typeof users>;
 
 export const InsertUserSchema = createInsertSchema(users);
-export const UserSchema = createSelectSchema(users);
+export const SelectUserSchema = createSelectSchema(users);
 
-export const UserProfileSchema = z.object({
+export const UserSchema = z.object({
     walletAddress: z.string(),
     username: z.string(),
     profilePictureUrl: z.string(),
     role: z.enum(['user', 'runner', 'admin'])
 });
-export type UserProfile = z.infer<typeof UserProfileSchema>;
+export type User = z.infer<typeof UserSchema>;
