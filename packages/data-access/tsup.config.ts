f135edfe2ee57798ed8e1370312ae0d46169b606
wkflanders <wflan@seas.upenn.edu>
2025-07-21 08:53:54 -0400

    build: add posts to models exports

--- packages/data-access/tsup.config.ts
+++ packages/data-access/tsup.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/db/client.ts', 'src/db/schema.ts', 'src/models/users.ts'],
+    entry: [
+        'src/db/client.ts',
+        'src/db/schema.ts',
+        'src/models/users.ts',
+        'src/models/posts.ts'
+    ],
     format: ['esm'],
     dts: true,
     clean: true,
