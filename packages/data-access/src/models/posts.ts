ea55ab20ec47a9d5eb7504d6d8c5e5a775b73f42
wkflanders <wflan@seas.upenn.edu>
2025-07-22 08:44:46 -0400

    fix: standardize posts models

--- packages/data-access/src/models/posts.ts
+++ packages/data-access/src/models/posts.ts
@@ -9,8 +9,7 @@ import {
 import { posts } from '../db/schema';
 
 import { UserSchema } from './users';
-
-// Repo Schemas
+import { RunPostSchema } from './runs';
 
 export const InsertPostSchema = createInsertSchema(posts);
 export const SelectPostSchema = createSelectSchema(posts);
@@ -19,43 +18,12 @@ export const UpdatePostSchema = createUpdateSchema(posts);
 export type InsertPost = InferInsertModel<typeof posts>;
 export type SelectPost = InferSelectModel<typeof posts>;
 
-// Service Schemas
-
-// export const UpdatePostSchema = z
-//     .object({
-//         id: z.number(),
-//         content: z.string().max(2000).optional(),
-//         visibility: z.enum(['public', 'hidden']).optional()
-//     })
-//     .refine(
-//         (data) => data.content !== undefined || data.visibility !== undefined
-//     );
-
-export const CreatePostSchema = z.object({
-    runId: z.string().min(1),
-    content: z.string().max(2000).optional(),
-    visibility: z.enum(['public', 'hidden']).default('public')
-});
-
-// Procedures Schemas
-
 export const PostSchema = z.object({
-    id: z.number(),
-    userId: z.number(),
-    runId: z.number(),
+    id: z.string(), // public id
     content: z.string().nullable(),
     visibility: z.enum(['public', 'hidden']),
-    isProfile: z.boolean(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
-    deletedAt: z.date().nullable(),
     user: UserSchema,
-    run: z.object({
-        id: z.number(),
-        distance: z.number(),
-        duration: z.number(),
-        startTime: z.date()
-    })
+    run: RunPostSchema
 });
 
 export type Post = z.infer<typeof PostSchema>;
