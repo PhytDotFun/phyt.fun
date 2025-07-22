044d78997d530f5a088e791828aa6f5bd613c9d8
wkflanders <wflan@seas.upenn.edu>
2025-07-22 08:43:48 -0400

    build: add posts and runs repos + services to DI

--- packages/trpc-adapters/src/di.ts
+++ packages/trpc-adapters/src/di.ts
@@ -2,35 +2,61 @@ import type { Dependencies as CoreDeps } from '@phyt/core/di';
 
 import { UserRepository } from './users/repository';
 import { UserService } from './users/service';
+import { PostRepository } from './posts/repository';
+import { PostService } from './posts/service';
+import { RunRepository } from './runs/repository';
+import { RunService } from './runs/service';
 
 export interface AppDependencies extends CoreDeps {
     userRepository: UserRepository;
     userService: UserService;
+    postRepository: PostRepository;
+    postService: PostService;
+    runRepository: RunRepository;
+    runService: RunService;
 }
 
 export function createAppDependencies(core: CoreDeps): AppDependencies {
-    const userRepository = new UserRepository(core);
+    const userRepository = new UserRepository({
+        db: core.db
+    });
 
     const userService = new UserService({
-        userRepository,
+        userRepository: userRepository,
         redis: core.redis
     });
 
-    // const postRepository = new PostRepository(core);
+    const runRepository = new RunRepository({
+        db: core.db
+    });
 
-    // const postService = new PostService({
-    //     postRepository,
-    //     redis: core.redis
-    // });
+    const runService = new RunService({
+        runRepository: runRepository,
+        redis: core.redis
+    });
+
+    const postRepository = new PostRepository({
+        db: core.db,
+        userRepository: userRepository
+    });
+
+    const postService = new PostService({
+        postRepository: postRepository,
+        userService: userService,
+        runService: runService,
+        redis: core.redis
+    });
 
     const repositories = {
-        userRepository
-        // postRepository
+        userRepository,
+        postRepository,
+        runRepository
     };
 
     const services = {
-        userService
-        // postService
+        userService,
+        postService,
+        runService
     };
 
     return {
