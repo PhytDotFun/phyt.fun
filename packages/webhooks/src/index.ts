dbe7ee2cbeab717da982b45b037add4731977c51
wkflanders <wflan@seas.upenn.edu>
2025-07-24 19:11:07 -0400

    fix: remove webhook barrel file

--- packages/webhooks/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { PrivyWebhookHandler } from './privy-handler';
-export {
-    webhookResponse,
-    IdempotencyManager,
-    WebhookHeadersSchema
-} from './helper';
-export type { WebhookResponse, WebhookHeaders } from './helper';
