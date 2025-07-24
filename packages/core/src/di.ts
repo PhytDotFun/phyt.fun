b624b53a797eee9b7a925e7fddc3bbac422e4715
wkflanders <wflan@seas.upenn.edu>
2025-07-24 18:47:32 -0400

    feat: expand DI interface

--- packages/core/src/di.ts
+++ packages/core/src/di.ts
@@ -2,8 +2,28 @@ import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import type { PrivyClient } from '@privy-io/server-auth';
 import type Redis from 'ioredis';
 
+import type {
+    Cache,
+    Idempotency,
+    QueueWithContext,
+    IdEncoder
+} from './contracts';
+
 export interface Dependencies {
     db: NodePgDatabase;
     redis: Redis;
+    bull: Redis;
+    cache: Cache;
+    idempotency: Idempotency;
     privy: PrivyClient;
+    authQueue: QueueWithContext;
+    postsQueue: QueueWithContext;
+    idEncoder: IdEncoder;
+    salts: {
+        users: string;
+        posts: string;
+        comments: string;
+        reactions: string;
+        runs: string;
+    };
 }
