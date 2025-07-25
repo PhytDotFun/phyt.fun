cd1f5ccbc84fde56c6b2b2933f3ef8b4e40b637e
wkflanders <wflan@seas.upenn.edu>
2025-07-25 16:38:01 -0400

    refactor: update DI to use plural svc, repo, and proc. names

--- packages/trpc-adapters/src/di.ts
+++ packages/trpc-adapters/src/di.ts
@@ -1,64 +1,64 @@
 import type { Dependencies as CoreDeps } from '@phyt/core/di';
 
-import { UserRepository } from './users/repository';
-import { UserService } from './users/service';
-import { PostRepository } from './posts/repository';
-import { PostService } from './posts/service';
-import { RunRepository } from './runs/repository';
-import { RunService } from './runs/service';
+import { UsersRepository } from './users/repository';
+import { UsersService } from './users/service';
+import { PostsRepository } from './posts/repository';
+import { PostsService } from './posts/service';
+import { RunsRepository } from './runs/repository';
+import { RunsService } from './runs/service';
 
 export interface AppDependencies extends CoreDeps {
-    userRepository: UserRepository;
-    userService: UserService;
-    postRepository: PostRepository;
-    postService: PostService;
-    runRepository: RunRepository;
-    runService: RunService;
+    usersRepository: UsersRepository;
+    usersService: UsersService;
+    postsRepository: PostsRepository;
+    postsService: PostsService;
+    runsRepository: RunsRepository;
+    runsService: RunsService;
 }
 
 export function createAppDependencies(core: CoreDeps): AppDependencies {
-    const userRepository = new UserRepository({
+    const usersRepository = new UsersRepository({
         db: core.db
     });
 
-    const userService = new UserService({
-        userRepository: userRepository,
+    const usersService = new UsersService({
+        usersRepository: usersRepository,
         redis: core.redis
     });
 
-    const runRepository = new RunRepository({
+    const runsRepository = new RunsRepository({
         db: core.db
     });
 
-    const runService = new RunService({
-        runRepository: runRepository,
+    const runsService = new RunsService({
+        runsRepository: runsRepository,
         redis: core.redis,
         idEncoder: core.idEncoder
     });
 
-    const postRepository = new PostRepository({
+    const postsRepository = new PostsRepository({
         db: core.db,
-        userRepository: userRepository
+        usersRepository: usersRepository
     });
 
-    const postService = new PostService({
-        postRepository: postRepository,
-        userService: userService,
-        runService: runService,
+    const postsService = new PostsService({
+        postsRepository: postsRepository,
+        usersService: usersService,
+        runsService: runsService,
         redis: core.redis,
         idEncoder: core.idEncoder
     });
 
     const repositories = {
-        userRepository,
-        postRepository,
-        runRepository
+        usersRepository,
+        postsRepository,
+        runsRepository
     };
 
     const services = {
-        userService,
-        postService,
-        runService
+        usersService,
+        postsService,
+        runsService
     };
 
     return {
