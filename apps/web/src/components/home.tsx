88303070177154455f78071f971a99bf05996109
wkflanders <wflan@seas.upenn.edu>
2025-07-26 21:12:23 -0400

    feat: add feed component to homepage

--- apps/web/src/components/home.tsx
+++ apps/web/src/components/home.tsx
@@ -1,5 +1,5 @@
 // import { HomeTrendingPanel } from '@/features/runners/ui/components/HomeTrendingPanel';
-// import { Feed } from '@/features/feed/components/ui/Feed';
+import { Feed } from '@/features/feed/ui/components/feed';
 // import { HomeCompetitionsPanel } from '@/features/competitions/ui/components/HomeCompetitionsPanel';
 
 export function Home() {
@@ -7,11 +7,11 @@ export function Home() {
         <div className="flex min-h-screen w-full">
             <div className="flex-1 pt-20">
                 <div className="grid grid-cols-1 lg:grid-cols-4 gap-0 h-full">
-                    {/* <div className="lg:col-span-2 border-r-2 border-border">
-                                <Feed />
-                            </div>
+                    <div className="lg:col-span-2 border-r-2 border-border">
+                        <Feed />
+                    </div>
 
-                            <div className="lg:col-span-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
+                    {/* <div className="lg:col-span-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
                                 <div className="border-r-2 border-border xl:border-r-2">
                                     <HomeCompetitionsPanel />
                                 </div>
