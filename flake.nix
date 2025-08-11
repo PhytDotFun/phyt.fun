912567d0bb9c07ff4d4bc748bdf6a76a9e66e44a
wkflanders <wflan@seas.upenn.edu>
2025-08-11 19:25:32 -0400

    feat: init nix and create flake.nix file

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "phyt dev env";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            pnpm
+            docker-compose
+            go-task
+            git
+            jq
+            yq
+            vault
+          ];
+        };
+      });
+}
+
+
