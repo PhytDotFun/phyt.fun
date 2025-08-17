50edaa512c0efc2f4ae820d5290be277cf1e05db
wkflanders <wflan@seas.upenn.edu>
2025-08-17 14:08:53 -0400

    feat: add additional nix packages

--- flake.nix
+++ flake.nix
@@ -4,27 +4,31 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { 
+      let pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            pre-commit
             nodejs_22
             pnpm
             docker-compose
             go
             go-task
+            foundry
             git
             jq
             yq
             python3
             vault
             cloudflared
+            shellcheck
+            shfmt
+            tflint
+            terraform
           ];
         };
       });
 }
-
-
