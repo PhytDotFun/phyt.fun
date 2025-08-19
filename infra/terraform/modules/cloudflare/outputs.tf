971b46a162aa29f6271eba1fa6085a2b36acdc68
wkflanders <wflan@seas.upenn.edu>
2025-08-19 13:17:31 -0400

    feat: terraform cloudflare module outputs

---
+++ infra/terraform/modules/cloudflare/outputs.tf
@@ -0,0 +1,15 @@
+output "tunnel_id" {
+    value = cloudflare_tunnel.staging.id
+    description = "Cloudflare tunnel ID"
+}
+
+output "tunnel_token" {
+    value = random_id.tunnel_scret.b64_std
+    sensitive = true
+    description = "Cloudflare tunnel token"
+}
+
+output "staging_url" {
+    value = "https://staging.phyt.fun"
+    description = "Staging environment URL"
+}
