a97e38a5b74729a12985e62b43ec8095a60c025f
wkflanders <wflan@seas.upenn.edu>
2025-08-19 13:18:52 -0400

    feat: terraform vault module variables

---
+++ infra/terraform/modules/vault/variables.tf
@@ -0,0 +1,9 @@
+variable "deployment_id" {
+    description = "Deployment identifier"
+    type = string
+}
+
+variable "instance_ip" {
+    description = "Instance IP address"
+    type = string
+}
