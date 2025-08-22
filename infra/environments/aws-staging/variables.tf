ab42b26586fdeb0911603c23ccd8b2b9b1012840
wkflanders <wflan@seas.upenn.edu>
2025-08-22 04:31:28 -0400

    feat: add DB compose to vault tf variables

--- infra/environments/aws-staging/variables.tf
+++ infra/environments/aws-staging/variables.tf
@@ -3,31 +3,31 @@
 # Only non-sensitive configuration values are variables
 
 variable "aws_region" {
-    description = "AWS region for resources"
-    type = string
-    default = "us-east-1"
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "deployment_id" {
-    description = "Unique deployment identifier (git sha)"
-    type = string
+  description = "Unique deployment identifier (git sha)"
+  type        = string
 }
 
 variable "instance_type" {
-    description = "EC2 instance type for staging"
-    type = string
-    default = "t4g.medium"
+  description = "EC2 instance type for staging"
+  type        = string
+  default     = "t4g.medium"
 }
 
 variable "spot_price" {
-    description = "Maximum spot price"
-    type = string
+  description = "Maximum spot price"
+  type        = string
 
-    default = "0.0336"
+  default = "0.0336"
 }
 
 variable "volume_size" {
-    description = "Root volume size in GB"
-    type = number
-    default = 30
+  description = "Root volume size in GB"
+  type        = number
+  default     = 30
 }
