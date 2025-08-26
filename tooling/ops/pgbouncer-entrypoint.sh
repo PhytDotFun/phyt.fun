9ec37bc072a045b14c311735b08086a653c99ee7
wkflanders <wflan@seas.upenn.edu>
2025-08-26 14:29:28 -0400

    fix: update pgnouncer entrypoint script

--- tooling/ops/pgbouncer-entrypoint.sh
+++ tooling/ops/pgbouncer-entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 export WAIT_FOR_SECRET_FILE="${WAIT_FOR_SECRET_FILE:-/vault/secrets/pgbouncer.ini}"
 /usr/local/bin/wait-for-secrets.sh sh -c '
-  exec /usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini
+  exec /usr/bin/pgbouncer /vault/secrets/pgbouncer.ini
 '
