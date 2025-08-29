a83827e120ca0aafffcaf752da91952e7653e032
wkflanders <wflan@seas.upenn.edu>
2025-08-29 13:38:48 -0400

    fix: update count for static secrets check

--- infra/terraform/modules/vault/data.tf
+++ infra/terraform/modules/vault/data.tf
@@ -10,13 +10,13 @@ data "vault_kv_secret_list" "workers_check" {
 }
 
 data "vault_kv_secret_v2" "hono_api_static" {
-  count = contains(data.vault_kv_secret_list.hono_api_check.names, "staging") ? 1 : 0
+  count = (contains(data.vault_kv_secret_list.hono_api_check.names, "staging") || contains(data.vault_kv_secret_list.hono_api_check.names, "staging/")) ? 1 : 0
   mount = "secret"
   name  = "hono-api/staging"
 }
 
 data "vault_kv_secret_v2" "workers_static" {
-  count = contains(data.vault_kv_secret_list.workers_check.names, "staging") ? 1 : 0
+  count = (contains(data.vault_kv_secret_list.workers_check.names, "staging") || contains(data.vault_kv_secret_list.workers_check.names, "staging/")) ? 1 : 0
   mount = "secret"
   name  = "workers/staging"
 }
