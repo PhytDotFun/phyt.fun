ca53682df83085795f80967d157d8fff02326f71
wkflanders <wflan@seas.upenn.edu>
2025-08-29 13:37:35 -0400

    fix: make wildcards more specific for ami

--- infra/environments/aws-staging/main.tf
+++ infra/environments/aws-staging/main.tf
@@ -57,12 +57,27 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-*-server-*arm64*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "image-type"
+    values = ["machine"]
   }
 }
 
