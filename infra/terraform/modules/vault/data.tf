8f31bc36aab1cf5a410585241da51defc5860565
wkflanders <wflan@seas.upenn.edu>
2025-08-30 11:42:36 -0400

    fix: update vault data block (syntx error)

--- infra/terraform/modules/vault/data.tf
+++ infra/terraform/modules/vault/data.tf
@@ -1,17 +1,17 @@
 # Check if static secrets exist before trying to read them
-data "vault_kv_secret_list" "secrets_check" {
+data "vault_kv_secrets_list_v2" "secrets_check" {
   mount = "secret"
   name  = ""
 }
 
 data "vault_kv_secret_v2" "hono_api_static" {
-  count = (contains(data.vault_kv_secret_list.secrets_check.names, "hono-api/staging") || contains(data.vault_kv_secret_list.secrets_check.names, "hono-api/staging/")) ? 1 : 0
+  count = (contains(data.vault_kv_secrets_list_v2.secrets_check.names, "hono-api/staging") || contains(data.vault_kv_secrets_list_v2.secrets_check.names, "hono-api/staging/")) ? 1 : 0
   mount = "secret"
   name  = "hono-api/staging"
 }
 
 data "vault_kv_secret_v2" "workers_static" {
-  count = (contains(data.vault_kv_secret_list.secrets_check.names, "workers/staging") || contains(data.vault_kv_secret_list.secrets_check.names, "workers/staging/")) ? 1 : 0
+  count = (contains(data.vault_kv_secrets_list_v2.secrets_check.names, "workers/staging") || contains(data.vault_kv_secrets_list_v2.secrets_check.names, "workers/staging/")) ? 1 : 0
   mount = "secret"
   name  = "workers/staging"
 }
