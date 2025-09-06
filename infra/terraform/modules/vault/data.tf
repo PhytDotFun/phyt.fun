3089b0dd9d96dd4c821f799095b2bf4d6c1921c3
wkflanders <wflan@seas.upenn.edu>
2025-09-06 00:41:00 -0400

    fix: remove secrets_check reference for vault tf data

--- infra/terraform/modules/vault/data.tf
+++ infra/terraform/modules/vault/data.tf
@@ -1,11 +1,9 @@
 data "vault_kv_secret_v2" "hono_api_static" {
-  count = (contains(data.vault_kv_secrets_list_v2.secrets_check.names, "hono-api/staging") || contains(data.vault_kv_secrets_list_v2.secrets_check.names, "hono-api/staging/")) ? 1 : 0
   mount = "secret"
   name  = "hono-api/staging"
 }
 
 data "vault_kv_secret_v2" "workers_static" {
-  count = (contains(data.vault_kv_secrets_list_v2.secrets_check.names, "workers/staging") || contains(data.vault_kv_secrets_list_v2.secrets_check.names, "workers/staging/")) ? 1 : 0
   mount = "secret"
   name  = "workers/staging"
 }
