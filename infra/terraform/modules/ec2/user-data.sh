0f8f398f9296d5639465fa83867251f49d91c4af
wkflanders <wflan@seas.upenn.edu>
2025-09-06 05:52:53 -0400

    ci: fix terraform escaping [skip build]

--- infra/terraform/modules/ec2/user-data.sh
+++ infra/terraform/modules/ec2/user-data.sh
@@ -110,7 +110,7 @@ while true; do
     # Attempt a graceful down with the staging profile
     if [ -d /opt/phyt ]; then
       cd /opt/phyt
-      docker compose --profile "${{COMPOSE_PROFILES:-staging}}" down || true
+      docker compose --profile "$${COMPOSE_PROFILES:-staging}" down || true
     fi
     break
   fi
