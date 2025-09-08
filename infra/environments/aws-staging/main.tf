c9c101de83b63567773b8c09a3e9925c8fe540ee
wkflanders <wflan@seas.upenn.edu>
2025-09-07 20:41:49 -0400

    ci: fix availability zone name/id mismatch [skip build]

--- infra/environments/aws-staging/main.tf
+++ infra/environments/aws-staging/main.tf
@@ -126,7 +126,7 @@ resource "aws_internet_gateway" "staging" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.staging.id
   cidr_block              = "10.100.1.0/24"
-  availability_zone       = data.aws_availability_zones.available.zone_ids[0]
+  availability_zone_id    = data.aws_availability_zones.available.zone_ids[0]
   map_public_ip_on_launch = true
 
   tags = {
