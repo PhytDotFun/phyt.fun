d370ca4e387fe9ff70736762ff68343e0c6eb42c
wkflanders <wflan@seas.upenn.edu>
2025-10-05 08:17:43 -0400

    ci: update tunnel tf outputs for cloudflare_dns_record resource [skip build]

--- infra/terraform/modules/dns/outputs.tf
+++ infra/terraform/modules/dns/outputs.tf
@@ -1,6 +1,6 @@
 output "record_id" {
   description = "Cloudflare record ID"
-  value       = cloudflare_record.tunnel.id
+  value       = cloudflare_dns_record.tunnel.id
 }
 
 output "hostname" {
@@ -10,10 +10,10 @@ output "hostname" {
 
 output "fqdn" {
   description = "Fully-qualified domain name"
-  value       = cloudflare_record.tunnel.hostname
+  value       = var.hostname
 }
 
 output "tunnel_endpoint" {
   description = "Tunnel endpoint this record points to"
-  value       = cloudflare_record.tunnel.content
+  value       = cloudflare_dns_record.tunnel.content
 }
