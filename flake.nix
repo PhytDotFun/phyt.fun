1a462f943f9a1a16edc9f191bbf3af3f0ecf8e32
wkflanders <wflan@seas.upenn.edu>
2025-10-10 10:35:39 -0400

    chore: add neon + update packages

--- flake.nix
+++ flake.nix
@@ -2,37 +2,41 @@
   description = "phyt dev env";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            pre-commit
-            nodejs
-            pnpm
-            docker-compose
-            go
-            go-task
-            foundry
-            git
-            jq
-            yq
-            ripgrep
-            fd
-            python3
-            vault
-            cloudflared
-            shellcheck
-            shfmt
-            tflint
-            hclfmt
-            terraform
-            packer
-          ];
-        };
-      });
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          pre-commit
+          nodejs
+          pnpm
+          docker-compose
+          go
+          go-task
+          foundry
+          git
+          jq
+          yq
+          ripgrep
+          fd
+          python3
+          vault
+          cloudflared
+          shellcheck
+          shfmt
+          tflint
+          hclfmt
+          terraform
+          packer
+        ];
+      };
+    });
 }
