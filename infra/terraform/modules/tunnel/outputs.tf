dd2afe5a508cdcecd611c54b980429294c95faac
wkflanders <wflan@seas.upenn.edu>
2025-10-12 15:45:50 -0400

    ci: fix tunnel token (apparently was using credentials format/encoding)

--- infra/terraform/modules/tunnel/outputs.tf
+++ infra/terraform/modules/tunnel/outputs.tf
@@ -10,12 +10,22 @@ output "tunnel_cname" {
 }
 
 output "tunnel_token" {
-  description = "Tunnel credentials JSON (base64 encoded for cloudflared)"
+  description = "Tunnel token for cloudflared (base64encoded)"
   value = base64encode(jsonencode({
-    AccountTag   = var.account_id
-    TunnelID     = cloudflare_zero_trust_tunnel_cloudflared.this.id
-    TunnelName   = cloudflare_zero_trust_tunnel_cloudflared.this.name
-    TunnelSecret = random_bytes.tunnel_secret.base64
+    a = var.account_id
+    t = cloudflare_zero_trust_tunnel_cloudflared.this.id
+    s = random_bytes.tunnel_secret.base64
   }))
   sensitive = true
 }
+
+# output "tunnel_credentials" {
+#   description = "Tunnel credentials JSON for credentials file"
+#   value = jsonencode({
+#     AccountTag   = var.account_id
+#     TunnelID     = cloudflare_zero_trust_tunnel_cloudflared.this.id
+#     TunnelName   = cloudflare_zero_trust_tunnel_cloudflared.this.name
+#     TunnelSecret = random_bytes.tunnel_secret.base64
+#   })
+#   sensitive = true
+# }
